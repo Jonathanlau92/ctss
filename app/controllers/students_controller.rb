b844e89c8f305f54967c0202d180268e27ea6fcf
Jonathanlau92 <jonathan@excide.co>
2020-04-14 18:00:50 +0800

    Add routes and controller methods

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,14 @@
 class StudentsController < ApplicationController
+  def new
+    @student = Student.new
+  end
+
+  def create
+    
+  end
+  
+  private
+  def student_params
+    params.require(:student).permit(:personal_consent, :full_name, :school_email, :alternate_email, :sex, :education_level, :parental_consent, :match_count, :subject_preferences)
+  end
 end
--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -1,2 +1,7 @@
 class TutorsController < ApplicationController
+  
+  private
+  def tutor_params
+    params.require(:tutor).permit(:personal_consent, :full_name, :school_email, :alternate_email, :sex, :hours_to_teach, :match_count, :subjects)
+  end
 end
