21107105c86695d221d53a7026fdc3d697e50ea3
Jonathanlau92 <jonathan@excide.co>
2020-04-20 22:43:47 +0800

    Complete tutor form

--- app/controllers/tutor_steps/build_controller.rb
+++ app/controllers/tutor_steps/build_controller.rb
@@ -9,6 +9,7 @@ class TutorSteps::BuildController < ApplicationController
 
   def update
     @tutor = Tutor.find(session[:tutor_id])
+    # Change to case statement when freer
     if params[:primary_subject_ids].present?
       # save subject into json
       @tutor.subjects["primary_subjects"] = params[:primary_subject_ids]
@@ -18,6 +19,8 @@ class TutorSteps::BuildController < ApplicationController
       @tutor.subjects["jc_subjects"] = params[:jc_subject_ids]
     elsif params[:tutor][:qualification].present?
       @tutor.update_attributes(qualification: params[:tutor][:qualification], past_experiences: params[:tutor][:past_experiences], reason_for_volunteering: params[:tutor][:reason_for_volunteering])
+    elsif params[:tutor][:personal_consent].present?
+      @tutor.update_attributes(personal_consent: params[:tutor][:personal_consent], code_of_conduct: params[:tutor][:code_of_conduct])
     else
       puts "error"
     end
