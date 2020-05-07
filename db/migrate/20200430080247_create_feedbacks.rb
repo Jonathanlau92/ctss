7924886bd57e0adf631dfb7af2dc66b282765eb6
cozinater <ray.tan180@gmail.com>
2020-05-07 15:46:40 +0700

    chnaged name attribute to full_name and topics to topics_covered in the feedback table

--- db/migrate/20200430080247_create_feedbacks.rb
+++ db/migrate/20200430080247_create_feedbacks.rb
@@ -3,7 +3,7 @@ class CreateFeedbacks < ActiveRecord::Migration[6.0]
     create_table :feedbacks do |t|
       t.datetime :timestamp
       t.string :matching_number
-      t.string :name
+      t.string :full_name
       t.text :action
       t.date :date_of_session
       t.time :start_time
@@ -11,7 +11,7 @@ class CreateFeedbacks < ActiveRecord::Migration[6.0]
       t.string :understand_concepts
       t.boolean :observe_code_of_conduct
       t.string :eleborate
-      t.text :topics
+      t.text :topics_covered
       t.boolean :comfortable_with_tutor
       t.text :other_feedbacks
       t.string :know_about_program
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_134705) do
+ActiveRecord::Schema.define(version: 2020_04_30_080247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_04_30_134705) do
   create_table "feedbacks", force: :cascade do |t|
     t.datetime "timestamp"
     t.string "matching_number"
-    t.string "name"
+    t.string "full_name"
     t.text "action"
     t.date "date_of_session"
     t.time "start_time"
@@ -29,15 +29,9 @@ ActiveRecord::Schema.define(version: 2020_04_30_134705) do
     t.text "topics_covered"
     t.boolean "comfortable_with_tutor"
     t.text "other_feedbacks"
-    t.string "know_about_programme"
-    t.string "platform"
-    t.text "difficulties_with_tutoring"
-    t.bigint "student_id", null: false
-    t.bigint "tutor_id", null: false
+    t.string "know_about_program"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["student_id"], name: "index_feedbacks_on_student_id"
-    t.index ["tutor_id"], name: "index_feedbacks_on_tutor_id"
   end
 
   create_table "students", force: :cascade do |t|
@@ -90,6 +84,4 @@ ActiveRecord::Schema.define(version: 2020_04_30_134705) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "feedbacks", "students"
-  add_foreign_key "feedbacks", "tutors"
 end
