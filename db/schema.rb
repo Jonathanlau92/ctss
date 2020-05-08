1547d26c446315e0efc2cae07fd4164cf5ab556f
Jonathanlau92 <jonathan@excide.co>
2020-05-08 22:49:55 +0800

    Add special request (special needs) to students table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_131119) do
+ActiveRecord::Schema.define(version: 2020_05_08_144601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_05_07_131119) do
     t.boolean "matched"
     t.boolean "sent_intro_email"
     t.text "topics_to_go_through"
+    t.string "special_request"
   end
 
   create_table "tutors", force: :cascade do |t|
