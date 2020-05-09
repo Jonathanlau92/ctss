698fbae791594aae5ffdf74ca4ff9402db71ee17
cozinater <ray.tan180@gmail.com>
2020-05-10 02:22:08 +0700

    Basic feedback form with wizard is completed. Only validation and match by matching number left

--- app/controllers/feedback_steps/build_controller.rb
+++ app/controllers/feedback_steps/build_controller.rb
@@ -10,6 +10,7 @@ class FeedbackSteps::BuildController < ApplicationController
 
   def update
   	@feedback = Feedback.find(session[:feedback_id])
-	render_wizard 
+  	@feedback.update_attributes(understand_concepts: params[:feedback][:understand_concepts], observe_code_of_conduct: params[:feedback][:observe_code_of_conduct], eleborate: params[:feedback][:eleborate], comfortable_with_tutor: params[:feedback][:comfortable_with_tutor], platform: params[:feedback][:platform], difficulties_with_tutoring: params[:feedback][:difficulties_with_tutoring], know_about_program: params[:feedback][:know_about_program], other_feedbacks: params[:feedback][:other_feedbacks])
+	render_wizard @feedback
   end
  end
