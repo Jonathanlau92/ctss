68574a7b21b50a994336e683f5fb777ac2cef5a0
Jonathanlau92 <jonathan@excide.co>
2020-05-10 20:49:19 +0800

    Production email setting and test email task

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,11 +1,11 @@
 class NotificationMailer < ApplicationMailer
-	def student_introductory_email(student)
-		@student = student
-		mail(to: @student.school_email, subject: 'CTSS Student Introductory Email')
-	end
+  def student_introductory_email(student)
+    @student = student
+    mail(to: @student.school_email, subject: 'CTSS Student Introductory Email')
+  end
 
-	def tutor_introductory_email(tutor)
-		@tutor = tutor
-		mail(to: @tutor.school_email, subject: 'CTSS Volunteer Introductory Email')
-	end
+  def tutor_introductory_email(tutor)
+    @tutor = tutor
+    mail(to: @tutor.school_email, subject: 'CTSS Volunteer Introductory Email')
+  end
 end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,5 +1,11 @@
 namespace :scheduler do
   desc "this task is to schedule matching to match student and tutor into a classroom"
-  task matching: :environment do
+  task send_test_email: :environment do
+    Student.all.each do |s|
+      NotificationMailer.student_introductory_email(s).deliver_now
+    end
+    Tutor.all.each do |t|
+      NotificationMailer.tutor_introductory_email(t).deliver_now
+    end
   end
 end
